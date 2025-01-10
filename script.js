3aae541383cdfde5f4bf4b49dd0e412150b8a0a1
CopperCoder227 <526csteiner@frhsd.com>
2025-01-10 11:45:25 -0500

    fixed immages - layout fixed for first 3 - hovers fixed for cards

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+function rockSelect() {
+    let myElem = document.querySelector("#Rock");
+    myElem.classList.add("selectedRock");
+    let otherButton1 = document.querySelector("#Paper");
+    let otherButton2 = document.querySelector("#Scissors");
+    otherButton1.classList.remove("selectedPaper");
+    otherButton2.classList.remove("selectedScissors");
+}
 
+function paperSelect() {
+    let myElem = document.querySelector("#Paper");
+    myElem.classList.add("selectedPaper");
+    let otherButton1 = document.querySelector("#Rock");
+    let otherButton2 = document.querySelector("#Scissors");
+    otherButton1.classList.remove("selectedRock");
+    otherButton2.classList.remove("selectedScissors");
+}
+
+function scissorsSelect() {
+    let myElem = document.querySelector("#Scissors");
+    myElem.classList.add("selectedScissors");
+    let otherButton1 = document.querySelector("#Paper");
+    let otherButton2 = document.querySelector("#Rock");
+    otherButton1.classList.remove("selectedPaper");
+    otherButton2.classList.remove("selectedRock");
+}
