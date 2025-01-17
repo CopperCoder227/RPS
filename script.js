81314f2ce0420e122f298cc04998af5e572b0f79
CopperCoder227 <526csteiner@frhsd.com>
2025-01-17 09:31:10 -0500

    jebguorts hojb

--- script.js
+++ script.js
@@ -33,6 +33,7 @@ function scissorsSelect() {
 //code to make game run//
 
 let [computerScore, userScore] = [0, 0];
+let counter = 0;
 
 const getComputerChoice = () => {
   const randomNumber = Math.floor(Math.random() * 3);
@@ -47,6 +48,10 @@ const getComputerChoice = () => {
 };
 
 const determineWinner = (userChoice, computerChoice) => {
+  let fLetter = userChoice.charAt(0).toUpperCase();
+  let lLetter = computerChoice.charAt(0).toUpperCase();
+  let url = `imgs/Vs/${fLetter}v${lLetter}.png`;
+  document.getElementById("chart").src = url;
   if (userChoice === computerChoice) {
     return "It's a draw!";
   } else if (userChoice === "rock") {
@@ -77,20 +82,32 @@ const determineWinner = (userChoice, computerChoice) => {
 };
 
 function playGame() {
-  if (userChoice) {
-    const computerChoice = getComputerChoice();
-    console.log(`You chose: ${userChoice}.`);
-    console.log(`Computer chose: ${computerChoice}.`);
-    document.getElementById("centerText").classList.add("d-none");
-    let columns = document.querySelectorAll("#result .row .col-4");
-    let col12 = document.getElementById("col12");
-    columns[0].textContent = `Mario chose: ${userChoice}.`;
-    col12.textContent = `${determineWinner(userChoice, computerChoice)}`;
-    columns[2].textContent = `Handaconda chose: ${computerChoice}.`;
-    let scoreColumns = document.getElementsByClassName("scores");
-    scoreColumns[0].textContent = `Mario's attacked: ${userScore} times!`;
-    scoreColumns[1].textContent = `Handaconda's attacked: ${computerScore} times!`;
+  if (counter <= 5) {
+    counter++;
+    if (userChoice) {
+      const computerChoice = getComputerChoice();
+      console.log(`You chose: ${userChoice}.`);
+      console.log(`Computer chose: ${computerChoice}.`);
+      document.getElementById("centerText").classList.add("d-none");
+      let columns = document.querySelectorAll("#result .row .col-4");
+      let col12 = document.getElementById("col12");
+      columns[0].textContent = `Mario chose: ${userChoice}.`;
+      col12.textContent = `${determineWinner(userChoice, computerChoice)}`;
+      columns[2].textContent = `Handaconda chose: ${computerChoice}.`;
+      let scoreColumns = document.getElementsByClassName("scores");
+      scoreColumns[0].textContent = `Mario's attacked: ${userScore} times!`;
+      scoreColumns[1].textContent = `Handaconda's attacked: ${computerScore} times!`;
+    }
+  } else {
+    (counter >= 5) {
+      let url = `imgs/Vs/${fLetter}v${lLetter}.png`;
+  document.getElementById("btn").src = url;
+    }
   }
 }
 
-//find way to make gane end after 5 rounds or 3 wins form either side. when that happens add replay button instead of contionue button. if mario ooses mke him fiant if handaconda faint make him explode into confetti
+//find way to make gane end after 5 rounds or 3 wins form either side. 
+// when that happens add replay button instead of contionue button. 
+// if mario ooses mke him fiant if handaconda faint make him explode into confetti 
+
+// fix xhart in css by adding or removing it 1chart / 2chart so that the base chart is different from the Vs img - ak rem
